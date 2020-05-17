7a9030a6cdd98fb741b622b006a6129814667967
VincentWillats <vincentwillats.software@gmail.com>
2020-05-17 16:34:25 +0800

    Moved MainPage to MVVM

---
+++ MobileApp/Views/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MobileApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+/* View for MainPage
+ * 
+ * 
+ * 
+ * Creator:         Vincent Willats
+ * Date Created:    28/03/2020
+ * Contact Email:   Vincentwillats.software@gmail.com
+ * 
+ * Changelog:
+ * 17/05/2020 -- Moved to MVVM
+ * 15/04/2020 -- Fixed double loading
+ * 07/03/2020 -- Working socials button
+ * 06/03/2020 -- Font all same size
+ * 05/03/2020 -- Working Leaderboard button
+ * 30/03/2020 -- Working Player Search button
+ * 29/03/2020 -- Working Recent Result button
+ * 28/03/2020 -- File made, Working Upcoming Tournament button
+ */
+
+namespace MobileApp
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel(this.Navigation);
+        }      
+    }
+}
